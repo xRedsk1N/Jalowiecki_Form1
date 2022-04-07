7a0b70ab2e344694ccec4ac3bb811ef164f2fd41
xRedsk1N <sz.jalowiecki@gmail.com>
2022-04-07 13:15:14 +0200

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent implements OnInit {
+  osoba = {
+    imie: '',
+    nazwisko: '',
+    telefon: null,
+    email: '',
+    odbiorca: '',
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  NowaOsoba() {}
+  onSubmit() {
+    console.log('submitted! ' + this.osoba.email);
+  }
 }
